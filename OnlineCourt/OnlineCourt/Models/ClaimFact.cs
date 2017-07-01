c55c82b0d99264a54df6cd2ef7a1fd460d4dce15
Ben Williams <ben.williams@pepperminttechnology.co.uk>
2017-07-01 17:20:32 +0100

    cdvdv

---
+++ OnlineCourt/OnlineCourt/Models/ClaimFact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCourt.Models
+{
+    public class ClaimFact
+    {
+        public string ClaimantClaim { get; set; }
+        public string ClaimantClaimDetectedLanguage { get; set; }
+        public decimal ClaimantClaimDetectedLanguageConfidence { get; set; }
+        public decimal ClaimantClaimDetectedKeyPhrases { get; set; }
+        public string DefendantResponse { get; set; }
+        public string DefendantResponseDetectedLanguage { get; set; }
+        public decimal DefendantResponseDetectedLanguageConfidence { get; set; }
+        public decimal DefendantResponseDetectedKeyPhrases { get; set; }
+        public string CaseOfficerComment { get; set; }
+    }
+}
