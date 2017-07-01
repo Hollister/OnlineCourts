8537063171b64753b8c9950235fef7d151687937
dpfoster <dan.foster@pepperminttechnology.co.uk>
2017-07-01 16:57:30 +0100

    fefwe

---
+++ OnlineCourt/OnlineCourt/Controllers/ClaimController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCourt.Controllers
+{
+    public class ClaimController : Controller
+    {
+        // GET: Claim
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Judge()
+        {
+            return View();
+        }
+    }
+}
